a9bc5ae8677e9529a74dac91551a7952b431a540
qiqibuchibailuobo <1103113563@qq.com>
2019-11-04 18:35:17 +0800

    commit4

--- src/main/java/com/yq/train/controller/indexController.java
+++ src/main/java/com/yq/train/controller/indexController.java
@@ -1,12 +1,14 @@
 package com.yq.train.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class indexController {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String login(){
-        return "hello";
+        return "index";
     }
 }
